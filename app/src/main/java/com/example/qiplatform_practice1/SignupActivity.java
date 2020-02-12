46cda8265211572e2a6107e5aaffb2484fc6d99d
seong0318 <seong0318@naver.com>
2020-02-12 10:33:38 +0900

    sign up 구현 완료

--- app/src/main/java/com/example/qiplatform_practice1/SignupActivity.java
+++ app/src/main/java/com/example/qiplatform_practice1/SignupActivity.java
@@ -22,21 +22,34 @@ import com.google.gson.GsonBuilder;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
+class Result {
+    private int result;
+
+    public Result(int result) {
+        this.result = result;
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
 
 interface RetrofitExService {
-    String url = new Url().getUrl();
-
     @GET("/usernamecheck/1")
-    Call<Object> getData(@Query("user_name") String username);
+    Call<Result> getData(@Query("user_name") String username);
 }
 
 class RetrofitClient {
@@ -44,6 +57,29 @@ class RetrofitClient {
 
     public static RetrofitExService getApiService() {
         return getInstance().create(RetrofitExService.class);
+    }
+
+    private static Retrofit getInstance() {
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+}
+
+interface RetrofitPost {
+    @FormUrlEncoded
+    @POST("/signuphandle/1")
+    Call<Result> postData(@FieldMap HashMap<String, Object> param);
+}
+
+class RetrofitSignup {
+    private static final String baseUrl = new Url().getUrl();
+
+    public static RetrofitPost getApiService() {
+        return getInstance().create(RetrofitPost.class);
     }
 
     private static Retrofit getInstance() {
@@ -77,23 +113,29 @@ public class SignupActivity extends AppCompatActivity implements Button.OnClickL
     @Override
     public void onClick(final View view) {
         final TextView usernameText = (TextView) findViewById(R.id.usn_edttxt);
+        TextView emailText = (TextView) findViewById(R.id.email_edttxt);
+        TextView pwdText = (TextView) findViewById(R.id.pwd_edttxt);
+        TextView pwdConfirmText = (TextView) findViewById(R.id.pwd_confirm_edttxt);
+        String username, email, pwd, pwd_confirm;
+        Call<Result> getResult;
+        HashMap<String, Object> formData;
+        Gson g = new Gson();
 
         switch (view.getId()) {
             case R.id.checkid_btn:
-                String username = usernameText.getText().toString();
-                Call<Object> getResult = RetrofitClient.getApiService().getData(username);
-                getResult.enqueue(new Callback<Object>() {
+                username = usernameText.getText().toString();
+                getResult = RetrofitClient.getApiService().getData(username);
+                getResult.enqueue(new Callback<Result>() {
                     @Override
-                    public void onResponse(Call<Object> call, Response<Object> response) {
+                    public void onResponse(Call<Result> call, Response<Result> response) {
                         if (response.isSuccessful()) {//
-                            int execResult = Integer.valueOf((String) response.body());
+                            int execResult = response.body().getResult();
 
                             if (execResult == 0) {
                                 showMessage("Usable username", "Please continue");
 
                                 Button button = (Button) findViewById(view.getId());
                                 button.setEnabled(false);
-
                                 usernameText.setEnabled(false);
                             } else if (execResult < 0) {
                                 showMessage("ERROR", "Query error");
@@ -104,13 +146,47 @@ public class SignupActivity extends AppCompatActivity implements Button.OnClickL
                     }
 
                     @Override
-                    public void onFailure(Call<Object> call, Throwable t) {
-                        Log.e("ERROR", "retrofit error");
+                    public void onFailure(Call<Result> call, Throwable t) {
+                        Log.e("ERROR", "username duplicate check retrofit error" + t.getMessage());
                     }
                 });
                 break;
             case R.id.signup_btn:
-                System.out.println("2");
+                username = usernameText.getText().toString();
+                email = emailText.getText().toString();
+                pwd = pwdText.getText().toString();
+                pwd_confirm = pwdConfirmText.getText().toString();
+
+                formData = new HashMap();
+                formData.put("user_name", username);
+                formData.put("email", email);
+                formData.put("pwd", pwd);
+
+                getResult = RetrofitSignup.getApiService().postData(formData);
+                getResult.enqueue(new Callback<Result>() {
+                    @Override
+                    public void onResponse(Call<Result> call, Response<Result> response) {
+                        if (response.isSuccessful()) {
+                            int execResult = response.body().getResult();
+//
+                            if (execResult == 0)
+                                showMessage("Successful Registration", "Please complete the account verification in the email provided.");
+                            else if (execResult == -1)
+                                showMessage("ERROR", "Store user Query error");
+                            else if (execResult == -2)
+                                showMessage("ERROR", "Store temp_user Query error");
+                            else if (execResult == -4)
+                                showMessage("ERROR", "Send mail error");
+                            else
+                                showMessage("ERROR", "Invalid access " + execResult);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Result> call, Throwable t) {
+                        Log.e("ERROR", "sign up retrofit error: " + t.getMessage());
+                    }
+                });
                 break;
         }
     }
