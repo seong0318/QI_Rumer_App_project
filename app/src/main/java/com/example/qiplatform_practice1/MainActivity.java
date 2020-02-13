66e58347bc5ffc165b20f0a9d3de8fa8d20187e0
hshwan <hshwan01072775108@gmail.com>
2020-02-12 23:56:23 -0800

    delete unnecessary things--2 (harrison)

--- app/src/main/java/com/example/qiplatform_practice1/MainActivity.java
+++ app/src/main/java/com/example/qiplatform_practice1/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 
-import com.example.qiplatform_practice1.ui.gallery.GalleryFragment;
 import com.example.qiplatform_practice1.ui.home.HomeFragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
